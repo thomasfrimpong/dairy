f7661b3474dd26dba083f67cc4b549ff930a855c
Thomas Frimpong <tfrimpong1987@gmail.com>
2021-08-16 16:47:38 +0000

    model for users

--- models/User.js
+++ models/User.js
@@ -1,24 +1,50 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: [true, "Please add a name"],
   },
   email: {
     type: String,
-    required: true,
+    match: [
+      /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/,
+      "Please enter a valid email.",
+    ],
+    required: [true, "Please add an email"],
     unique: true,
   },
 
   password: {
     type: String,
-    required: true,
+    required: [true, "Please add a password"],
+    minlength: 6,
+    select: false,
   },
   date: {
     type: Date,
     default: Date.now,
   },
 });
+
+//Encrypting password before storing in db
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+//Create JWT and return it
+UserSchema.methods.getSignedJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
+
+//match entered password to hashed password in database
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 module.exports = mongoose.model("User", UserSchema);
